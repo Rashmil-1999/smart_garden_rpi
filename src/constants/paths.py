18b90f1db2a914992a2ff55d19eb0b3eaccbb823
rashmil-1999 <damonsalvatore833@gmail.com>
2022-03-29 10:55:09 -0500

    Path change for RPi

--- src/constants/paths.py
+++ src/constants/paths.py
@@ -1,9 +1,9 @@
 import pathlib
 
-# BASE_DIR: pathlib.Path = pathlib.Path("/home/pi/Desktop/smart_garden")
-BASE_DIR: pathlib.Path = pathlib.Path(
-    "/Users/rashmilpanchani/Documents/Projects/smart_garden/src"
-)
+BASE_DIR: pathlib.Path = pathlib.Path("/home/pi/Desktop/smart_garden_rpi")
+# BASE_DIR: pathlib.Path = pathlib.Path(
+#     "/Users/rashmilpanchani/Documents/Projects/smart_garden/src"
+# )
 LOGS: pathlib.Path = BASE_DIR / "logs"
 CONSTANTS: pathlib.Path = BASE_DIR / "constants"
 
