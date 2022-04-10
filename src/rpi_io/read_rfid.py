6b51ba013d2f6644cd77265e14645d32818c0faf
rashmil-1999 <damonsalvatore833@gmail.com>
2022-04-09 21:31:32 -0500

    add rfid reader

--- src/rpi_io/read_rfid.py
+++ src/rpi_io/read_rfid.py
@@ -14,5 +14,6 @@ reader = telnetlib.Telnet(RFID_READER, RFID_PORT)
 data: List[str] = reader.read_until(b" ", timeout=2).decode().split("\r\n")
 clean_data: List[dict] = [json.loads(entry) for entry in data if entry != ""]
 
-pprint(clean_data)
+pprint([c_d["tid"] for c_d in clean_data])
+pprint(len(clean_data))
 
