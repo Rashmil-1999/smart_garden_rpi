c049c05c009813394f58b8de5c6538f61f0514ae
Sam Schlesinger <samuelsam93@gmail.com>
2022-04-23 11:34:33 -0500

    reverting last change

--- src/rpi_io/read_rfid.py
+++ src/rpi_io/read_rfid.py
@@ -11,7 +11,7 @@ from constants import RFID_READER, RFID_PORT
 
 reader = telnetlib.Telnet(RFID_READER, RFID_PORT)
 
-data: List[str] = reader.read_until(b"\n", timeout=2).decode().split("\r\n")
+data: List[str] = reader.read_until(b" ", timeout=2).decode().split("\r\n")
 clean_data: List[dict] = [json.loads(entry) for entry in data if entry != ""]
 
 pprint(clean_data)
